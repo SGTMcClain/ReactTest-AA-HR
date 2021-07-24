269b23bf8e8de336ad460ed3cdb05202d3f56134
Nathan McClain <n8mcclain@gmail.com>
2021-07-24 09:42:53 -0400

    I'm struggling with understanding how the react router works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React  from 'react';
 import './App.css';
 import NavBar from './components/nav-bar/index.js';
 import 'h8k-components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
 
 const title = "Navigation Bar";
 
@@ -9,7 +12,14 @@ function App() {
   return (
     <div>
       <h8k-navbar header={title} />
-      <NavBar/>
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route path='/' component={props => <Home {...props} />} />
+          <Route path='/about' component={props => <About {...props} />} />
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -1,21 +1,26 @@
 import React  from "react";
 import "./index.css";
+import App from '../../App'
+import ReactDOM from 'react-dom';
 
 export default function NavBar() {
 
+  
   return (
     <div className="layout-column justify-content-center align-items-center">
       <div className="layout-row justify-content-around align-items-center mt-20 links"
            data-testid="navigation-tabs">
-          <a>Home</a>
+          <a to='/'>Home</a>
           <a>News</a>
           <a>Contact</a>
-          <a>About</a>
+          <a to='/about'>About</a>
       </div>
 
       <div className="card w-20 ma-0">
         <section className="card-text" data-testid="tab-content">
-          <span>PAGE</span>
+          <span id='output'>
+            **ROUTES GO HERE**
+          </span>
         </section>
       </div>
     </div>
